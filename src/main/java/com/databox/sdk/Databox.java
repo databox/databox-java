27c38304b1c9ad5a98359b1ed6c2ae39f5a463bf
Oto Brglez <otobrglez@gmail.com>
2016-01-25 22:11:53 +0100

    And this.

--- src/main/java/com/databox/sdk/Databox.java
+++ src/main/java/com/databox/sdk/Databox.java
@@ -7,8 +7,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.*;
-import java.nio.charset.Charset;
+
 import java.text.SimpleDateFormat;
+
 import java.util.*;
 
 import org.slf4j.Logger;
